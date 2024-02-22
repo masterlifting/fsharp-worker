6c44851f2aebe7d12ad00c1d1282840193c40e87
Andrey Pestunov <masterlifting@gmail.com>
2024-02-22 22:38:15 +0100

    Add configuration file and worker logic

--- src/Program.fs
+++ src/Program.fs
@@ -1,1 +1,31 @@
-﻿
+﻿open System
+open System.Threading
+open Microsoft.Extensions.Configuration
+
+let getConfig() =
+    let builder: IConfigurationBuilder = new ConfigurationBuilder()
+    builder.AddJsonFile("appsettings.json", optional = false, reloadOnChange = true)
+        |> fun c -> c.Build()
+        |> fun c -> c.GetSection("Tasks23").Value
+
+let runTask config =
+    printfn $"Task started with config: {config}"
+
+let startWoker (cToken: CancellationToken) =
+    async {
+        let period = TimeSpan.FromSeconds(5.)
+        let timer = new PeriodicTimer(period)
+        while true do
+            let!_ = timer.WaitForNextTickAsync(cToken).AsTask() |> Async.AwaitTask
+            getConfig |> runTask
+    }
+
+[<EntryPoint>]
+let main _ =
+    let cts = new CancellationTokenSource()
+    
+    cts.Token
+        |> startWoker
+        |> Async.RunSynchronously
+
+    0

7716a4ed733d7e3ab8b12ea0a5598f5dc244503b
Andrei Pestunov <andrei.pestunov@akvelon.com>
2024-04-09 13:12:14 +0200

    Refactor startWorker function in Program.fs to use pipeline operator and improve code readability Refactor file paths in Worker.fsproj and Repository.fs Refactor startWorker function in Core.fs to use pattern matching instead of getHandler function Refactor handleSteps function in Core.fs to improve code readability and logging consistency Refactor handleSteps function in Core.fs to improve code readability and logging consistency Refactor Serialization module in src/Serialization.fs to remove unused code Refactor TaskStepLog type in Domain.fs to use more descriptive field names and update references in related modules Refactor handleSteps function in Core.fs to improve code readability and logging consistency Refactor Mapper module in Mapper.fs to use more descriptive function names and move TaskStep related functions to a separate module Refactor Mapper module in Mapper.fs to use more descriptive function names

--- src/Program.fs
+++ src/Program.fs
@@ -4,22 +4,24 @@ open Domain.Core
 [<EntryPoint>]
 let main args =
 
-    let duration =
-        match args.Length with
-        | 1 ->
-            match args.[0] with
-            | DSL.AP.IsFloat seconds -> seconds
-            | _ -> (TimeSpan.FromDays 1).TotalSeconds
-        | _ -> (TimeSpan.FromDays 1).TotalSeconds
-
     match Repository.getTasks () with
     | Error error -> $"Error: {error}" |> Log.error
     | Ok tasks ->
-        { Duration = duration
-          Tasks = tasks
-          Handlers = TaskStepHandlers.taskHandlers
-          getTask = Repository.getTask }
-        |> Core.startWorker
-        |> Async.RunSynchronously
+
+        let duration =
+            match args.Length with
+            | 1 ->
+                match args.[0] with
+                | DSL.AP.IsFloat seconds -> seconds
+                | _ -> (TimeSpan.FromDays 1).TotalSeconds
+            | _ -> (TimeSpan.FromDays 1).TotalSeconds
+
+        let config =
+            { Duration = duration
+              Tasks = tasks
+              Handlers = TaskStepHandlers.taskHandlers
+              getTask = Repository.getTask }
+
+        config |> Core.startWorker |> Async.RunSynchronously
 
     0

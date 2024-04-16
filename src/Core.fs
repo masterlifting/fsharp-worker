313751ebdb59343318edeccef34720d1a0fc7811
Andrei Pestunov <andrei.pestunov@akvelon.com>
2024-04-16 15:30:30 +0200

    Refactor logging functions in Logging.fs to include log messages

--- src/Core.fs
+++ src/Core.fs
@@ -5,6 +5,7 @@ open System.Threading
 open Domain
 open Domain.Core
 open Worker
+open Infrastructure.Logging
 
 let rec private handleSteps taskName (steps: TaskStep list) (ct: CancellationToken) =
     async {
@@ -39,11 +40,11 @@ let rec private handleSteps taskName (steps: TaskStep list) (ct: CancellationTok
 
 and private handleStep taskName step ct =
     async {
-        $"Task '%s{taskName}'. Step '%s{step.Name}'. Started" |> Logger.info
+        $"Task '%s{taskName}'. Step '%s{step.Name}'. Started" |> Log.info
 
         match! step.Handle() with
-        | Error error -> $"Task '%s{taskName}'. Step '%s{step.Name}'. Failed. %s{error}" |> Logger.error
-        | Ok msg -> $"Task '%s{taskName}'. Step '%s{step.Name}'. Completed. %s{msg}" |> Logger.debug
+        | Error error -> $"Task '%s{taskName}'. Step '%s{step.Name}'. Failed. %s{error}" |> Log.error
+        | Ok msg -> $"Task '%s{taskName}'. Step '%s{step.Name}'. Completed. %s{msg}" |> Log.debug
 
         do! handleSteps taskName step.Steps ct
     }
@@ -72,23 +73,22 @@ let rec private startTask
     =
     async {
         match! getTask taskName with
-        | Error error -> $"Task '%s{taskName}'. Failed. %s{error}" |> Logger.error
+        | Error error -> $"Task '%s{taskName}'. Failed. %s{error}" |> Log.error
         | Ok task ->
             let! taskCt = Scheduler.getExpirationToken taskName task.Scheduler
 
             match taskCt.IsCancellationRequested with
-            | true -> $"Task '%s{taskName}'. Stopped" |> Logger.warning
+            | true -> $"Task '%s{taskName}'. Stopped" |> Log.warning
             | false ->
                 match mergeStepsHandlers task.Steps handler.Steps with
-                | Error error -> $"Task '%s{taskName}'. Failed. %s{error}" |> Logger.error
+                | Error error -> $"Task '%s{taskName}'. Failed. %s{error}" |> Log.error
                 | Ok steps ->
 
-                    $"Task '%s{taskName}'. Started" |> Logger.info
+                    $"Task '%s{taskName}'. Started" |> Log.info
                     do! handleSteps taskName steps workerCt
-                    $"Task '%s{taskName}'. Completed" |> Logger.debug
+                    $"Task '%s{taskName}'. Completed" |> Log.debug
 
-                    $"Task '%s{taskName}'. Next run will be in {task.Scheduler.Delay}"
-                    |> Logger.trace
+                    $"Task '%s{taskName}'. Next run will be in {task.Scheduler.Delay}" |> Log.trace
 
                     do! Async.Sleep task.Scheduler.Delay
                     do! startTask taskName handler workerCt getTask
@@ -96,22 +96,26 @@ let rec private startTask
 
 let startWorker config =
     async {
-        $"The worker will be running for %f{config.Duration} seconds" |> Logger.warning
-        use cts = new CancellationTokenSource(TimeSpan.FromSeconds config.Duration)
+        match! config with
+        | Error error -> error |> Log.error
+        | Ok config ->
 
-        let! result =
-            config.Tasks
-            |> Seq.map (fun task ->
-                match config.Handlers |> Seq.tryFind (fun x -> x.Name = task.Name) with
-                | Some handler -> startTask task.Name handler cts.Token config.getTask
-                | None -> async { return $"Task '%s{task.Name}'. Failed. Handler was not found" |> Logger.error })
-            |> Async.Parallel
-            |> Async.Catch
+            $"The worker will be running for %f{config.Duration} seconds" |> Log.warning
+            use cts = new CancellationTokenSource(TimeSpan.FromSeconds config.Duration)
 
-        match result with
-        | Choice1Of2 _ -> $"All tasks completed successfully" |> Logger.info
-        | Choice2Of2 ex ->
-            match ex with
-            | :? OperationCanceledException -> $"Worker was stopped" |> Logger.warning
-            | _ -> $"Worker failed. %s{ex.Message}" |> Logger.error
+            let! result =
+                config.Tasks
+                |> Seq.map (fun task ->
+                    match config.Handlers |> Seq.tryFind (fun x -> x.Name = task.Name) with
+                    | Some handler -> startTask task.Name handler cts.Token config.getTask
+                    | None -> async { return $"Task '%s{task.Name}'. Failed. Handler was not found" |> Log.error })
+                |> Async.Parallel
+                |> Async.Catch
+
+            match result with
+            | Choice1Of2 _ -> $"All tasks completed successfully" |> Log.info
+            | Choice2Of2 ex ->
+                match ex with
+                | :? OperationCanceledException -> $"Worker was stopped" |> Log.warning
+                | _ -> $"Worker failed. %s{ex.Message}" |> Log.error
     }
--- src/Scheduler.fs
+++ src/Scheduler.fs
@@ -3,7 +3,7 @@ module internal Worker.Scheduler
 open System
 open System.Threading
 open Domain.Core
-open Worker
+open Infrastructure.Logging
 
 let getExpirationToken taskName scheduler =
     async {
@@ -11,7 +11,7 @@ let getExpirationToken taskName scheduler =
         let cts = new CancellationTokenSource()
 
         if not scheduler.IsEnabled then
-            $"Task '%s{taskName}' is disabled" |> Logger.warning
+            $"Task '%s{taskName}' is disabled" |> Log.warning
             do! cts.CancelAsync() |> Async.AwaitTask
 
         if not cts.IsCancellationRequested then
@@ -19,7 +19,7 @@ let getExpirationToken taskName scheduler =
             | Some stopWork ->
                 match stopWork - now with
                 | delay when delay > TimeSpan.Zero ->
-                    $"Task '%s{taskName}' will be stopped at {stopWork}" |> Logger.warning
+                    $"Task '%s{taskName}' will be stopped at {stopWork}" |> Log.warning
                     cts.CancelAfter delay
                 | _ -> do! cts.CancelAsync() |> Async.AwaitTask
             | _ -> ()
@@ -27,12 +27,12 @@ let getExpirationToken taskName scheduler =
         if not cts.IsCancellationRequested then
             match scheduler.StartWork - now with
             | delay when delay > TimeSpan.Zero ->
-                $"Task '%s{taskName}' will start at {scheduler.StartWork}" |> Logger.warning
+                $"Task '%s{taskName}' will start at {scheduler.StartWork}" |> Log.warning
                 do! Async.Sleep delay
             | _ -> ()
 
             if scheduler.IsOnce then
-                $"Task '%s{taskName}' will be run once" |> Logger.warning
+                $"Task '%s{taskName}' will be run once" |> Log.warning
                 cts.CancelAfter(scheduler.Delay.Subtract(TimeSpan.FromSeconds 1.0))
 
         return cts.Token

bea19901c6cff35570ee86c70e105e5b4354c73e
Andrei Pestunov <andrei.pestunov@akvelon.com>
2024-08-27 18:59:20 +0200

    Extend ConfirmationOption, refactor logging, update configs
    Extended ConfirmationOption with LastAvailable in Domain.fs.
    Refactored Mapper.fs modules to use string interpolation for
    error messages. Updated Core.fs to handle new option and
    improve error message formatting. Commented out specific
    task handlers in Program.fs. Modified appsettings.yaml to
    adjust durations and comment out configurations. Added new
    run' function in Russian.fs. Refactored Scheduler.fs to
    improve logging and handle optional parameters more robustly.

--- src/Scheduler.fs
+++ src/Scheduler.fs
@@ -9,12 +9,14 @@ open Worker.Domain
 let private now timeShift =
     DateTime.UtcNow.AddHours(timeShift |> float)
 
-let private checkWorkday (cts: CancellationTokenSource) (task: Task) timeShift (workdays: Set<DayOfWeek>) =
+let private checkWorkday
+    ((cts: CancellationTokenSource), (task: Task), (timeShift: int8), (workdays: Set<DayOfWeek>), count)
+    =
     async {
         let now = now timeShift
 
         if now.DayOfWeek |> Set.contains >> not <| workdays then
-            let message = $"Task '%s{task.Name}'. Today is not a working day."
+            let message = $"Task '%i{count}.%s{task.Name}'. Today is not a working day."
 
             match task.Recursively with
             | None ->
@@ -26,17 +28,28 @@ let private checkWorkday (cts: CancellationTokenSource) (task: Task) timeShift (
                 do! Async.Sleep delay
     }
 
-let private checkLimit (cts: CancellationTokenSource) (task: Task) timeShift (limit: uint option) count =
+let private checkLimit
+    ((cts: CancellationTokenSource), (task: Task), (timeShift: int8 option), (limit: uint option), count)
+    =
     async {
         match limit with
         | Some limit when count % (limit + 1u) = 0u ->
-            let message = $"Task '%s{task.Name}'. Limit {limit} reached"
+            let message = $"Task '%i{count}.%s{task.Name}'. Limit {limit} reached"
 
             match task.Recursively with
             | None ->
                 $"{message}." |> Log.warning
                 do! cts.CancelAsync() |> Async.AwaitTask
             | Some _ ->
+                let timeShift =
+                    match timeShift with
+                    | Some timeShift -> timeShift
+                    | None ->
+                        $"Task '%i{count}.%s{task.Name}'. TimeShift is not defined. Default value 0 will be used."
+                        |> Log.warning
+
+                        0y
+
                 let now = now timeShift
                 let delay = now.Date.AddDays(1.) - now
                 let formattedDelay = delay.ToString("dd\\d\\ hh\\h\\ mm\\m\\ ss\\s")
@@ -48,23 +61,27 @@ let private checkLimit (cts: CancellationTokenSource) (task: Task) timeShift (li
         | _ -> ()
     }
 
-let private tryStopWork (cts: CancellationTokenSource) (task: Task) timeShift (stopWork: DateTime option) =
+let private tryStopWork
+    ((cts: CancellationTokenSource), (task: Task), (timeShift: int8), (stopWork: DateTime option), count)
+    =
     async {
         match stopWork with
         | Some stopWork ->
             match stopWork - now timeShift with
             | delay when delay > TimeSpan.Zero ->
-                $"Task '%s{task.Name}'. Will be stopped at {stopWork}." |> Log.warning
+                $"Task '%i{count}.%s{task.Name}'. Will be stopped at {stopWork}." |> Log.warning
                 cts.CancelAfter delay
             | _ -> do! cts.CancelAsync() |> Async.AwaitTask
         | _ -> ()
     }
 
-let private tryStartWork (task: Task) timeShift (startWork: DateTime) =
+let private tryStartWork ((task: Task), (timeShift: int8), (startWork: DateTime), count) =
     async {
         match startWork - now timeShift with
         | delay when delay > TimeSpan.Zero ->
-            $"Task '%s{task.Name}'. Will be started at {startWork}." |> Log.warning
+            $"Task '%i{count}.%s{task.Name}'. Will be started at {startWork}."
+            |> Log.warning
+
             do! Async.Sleep delay
         | _ -> ()
     }
@@ -72,19 +89,19 @@ let private tryStartWork (task: Task) timeShift (startWork: DateTime) =
 let getExpirationToken (task: Task) count (cts: CancellationTokenSource) =
     async {
         match task.Schedule with
-        | None -> return cts.Token
+        | None -> do! checkLimit (cts, task, None, task.Limit, count)
         | Some schedule ->
 
-            do! checkLimit cts task schedule.TimeShift task.Limit count
+            do! checkLimit (cts, task, Some schedule.TimeShift, task.Limit, count)
 
             if cts.Token |> notCanceled then
-                do! tryStopWork cts task schedule.TimeShift schedule.StopWork
+                do! tryStopWork (cts, task, schedule.TimeShift, schedule.StopWork, count)
 
             if cts.Token |> notCanceled then
-                do! checkWorkday cts task schedule.TimeShift schedule.Workdays
+                do! checkWorkday (cts, task, schedule.TimeShift, schedule.Workdays, count)
 
             if cts.Token |> notCanceled then
-                do! tryStartWork task schedule.TimeShift schedule.StartWork
+                do! tryStartWork (task, schedule.TimeShift, schedule.StartWork, count)
 
-            return cts.Token
+        return cts.Token
     }
